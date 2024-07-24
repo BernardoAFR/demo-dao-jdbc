7b3c31d806a19433a05f4f63da238bd732b0ba06
Bernardo Rodrigues <bernardofigueiredo387@gmail.com>
2024-07-24 14:23:24 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,20 +2,28 @@ package application;
 
 import java.util.Date;
 
+import model.DAO.DAOFactory;
+import model.DAO.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Main program running...");
+
+		/*
+		 * Department obj = new Department(1, "Books"); Seller seller = new Seller(21,
+		 * "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		 */
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		SellerDAO sellerdao = DAOFactory.createSellerDao();
+
+		Seller seller = sellerdao.findById(3);
 		
 		System.out.println(seller);
-		//System.out.println(obj);
-	
+		// System.out.println(obj);
+
 	}
 
 }
--- src/model/DAO/DAOFactory.java
+++ src/model/DAO/DAOFactory.java
@@ -1,5 +1,11 @@
 package model.DAO;
 
-public class DAOFactory {
+import db.DB;
+import model.DAO.impl.SellerDAOJDBC;
 
+public class DAOFactory {
+	
+	public static  SellerDAO createSellerDao() {
+		return new SellerDAOJDBC(DB.getConnection());
+	}
 }
